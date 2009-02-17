eeadd40fa08bffbc3b1fd3c5916d6f6889f11424
Hugh Evans <hugh@artpop.com.au>
2009-02-18 00:02:44 +1100

    Big bang

---
+++ hughevans.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'yaml'
+require 'sinatra'
+require 'haml'
+
+ARTICLES = YAML::load(File.read(File.join(Sinatra.application.options.root, 'articles.yml')))
+
+helpers do
+  def article(article)
+    haml(:"articles/_#{article['slug']}", :layout => false)
+  end
+end
+
+get '/' do
+  haml :home
+end
+
+get '/:style.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :"stylesheets/#{params[:style]}"
+end
+
+get '/:year/:month/:day/:slug' do
+  @article = ARTICLES[params[:slug]] || raise(Sinatra::NotFound)
+  @single_view = true
+  haml :article
+end
+
+# I'll have my preserved text served escaped not stired..
+module Haml::Filters::Preserve
+  include Haml::Filters::Base
+
+  def render(text)
+    Haml::Helpers.preserve(Haml::Helpers.html_escape(text))
+  end
+end
